eb62f4e1ce586098336d680c252584ad5f88303c
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-03 19:00:11 +0100

    Add error handling

--- FirstDatabase/Testing/DataReader.cs
+++ FirstDatabase/Testing/DataReader.cs
@@ -15,20 +15,46 @@ namespace Testing
             this.Officer = officer;
             this.GetDataFromCSV(path);
         }
+
         private void GetDataFromCSV(string path)
         {
             using StreamReader streamReader = new StreamReader(path);
             string line;
             while((line = streamReader.ReadLine())!=null)
             {
-                string[] row = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                string vehicleID = row[0];
-                string passport = row[1];
-                string note = row[2];
-                DateTime ticketDay = DateTime.Parse(row[3]);
-                string name = row[4];
-                this.Tickets.Add(new Ticket(name, vehicleID, passport, note, ticketDay, this.Officer));
+                if(IsValid(line))
+                {
+                    string[] row = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                    string vehicleID = row[0];
+                    string passport = row[1];
+                    string note = row[2];
+                    DateTime ticketDay = DateTime.Parse(row[3]);
+                    string name = row[4];
+
+                    this.Tickets.Add(new Ticket(name, vehicleID, passport, note, ticketDay, this.Officer));
+                }                
             }
+        }
+
+        /// <summary>
+        /// Check if input data is valid
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private static bool IsValid(string line)
+        {
+            string[] row = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            
+            if(row.Length !=5)
+            {
+                return false;
+            }
+
+            if(!DateTime.TryParse(row[3],out _))
+            {
+                return false;
+            }
+            return true;
         }
         public void PrintData()
         {
--- FirstDatabase/Testing/Program.cs
+++ FirstDatabase/Testing/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 
 namespace Testing
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string path = @"..\..\..\..\..\csv\data.csv";
             DataReader reader = new DataReader("TRUNG", path);
@@ -14,21 +15,37 @@ namespace Testing
 
             //Console.WriteLine("Enter connection string:");
             string connectionString = "Server = localhost; Database = TutorialDB; Trusted_Connection = True;";
-            
-            using(SqlConnection conn = new SqlConnection())
-            {
-                conn.ConnectionString = connectionString;
-                //conn.Open();
-                //InsertData(conn, reader);
-            }
 
+            //Insert data to table from csv file
+            InsertData(connectionString, reader);
         }
-        private static void InsertData(SqlConnection connection, DataReader data)
+
+        private static void InsertData(string connectionString, DataReader data)
         {
-            foreach(var ticket in data.Tickets)
+            StringBuilder errorMessages = new StringBuilder();
+            try
             {
-                InsertData(connection, ticket.GuidID, ticket.Name, ticket.VehicleID, ticket.Passport, ticket.Notes, ticket.TicketDay, ticket.TicketIssue, ticket.Officer);
+                using SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                foreach (var ticket in data.Tickets)
+                {
+                    InsertData(connection, ticket.GuidID, ticket.Name, ticket.VehicleID, ticket.Passport, ticket.Notes, ticket.TicketDay, ticket.TicketIssue, ticket.Officer);
+                }
             }
+            catch (SqlException ex)
+            {
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                Console.WriteLine(errorMessages.ToString());
+            }
+            
         }
 
         private static void InsertData(SqlConnection connection, Guid guid, string name, string vehicleId, string passport, string note, DateTime ticketDay, DateTime ticketIssue, string officer)
@@ -38,7 +55,7 @@ namespace Testing
             {
                 querryInsert.Connection = connection;
                 querryInsert.Parameters.Add("@guid", SqlDbType.UniqueIdentifier).Value = guid;
-                querryInsert.Parameters.Add("@name", SqlDbType.NVarChar,50).Value = name;
+                querryInsert.Parameters.Add("@name", SqlDbType.NVarChar, 50).Value = name;
                 querryInsert.Parameters.Add("@vehicleId", SqlDbType.NVarChar, 50).Value = vehicleId;
                 querryInsert.Parameters.Add("@passport", SqlDbType.NVarChar, 50).Value = passport;
                 querryInsert.Parameters.Add("@note", SqlDbType.NVarChar, 250).Value = note;
@@ -48,9 +65,6 @@ namespace Testing
 
                 querryInsert.ExecuteNonQuery();
             }
-
         }
-
-        
     }
 }
