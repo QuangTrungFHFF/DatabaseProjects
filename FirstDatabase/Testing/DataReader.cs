10791aa75b427cccffd732c93887940391505c95
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-03 18:04:13 +0100

    Add new class Ticket

--- FirstDatabase/Testing/DataReader.cs
+++ FirstDatabase/Testing/DataReader.cs
@@ -7,23 +7,11 @@ namespace Testing
 {
     class DataReader
     {
-        public List<Guid> GuidID { get; private set; }
-        public List<string> Names { get; private set; }
-        public List<string> VehicleIDs { get; private set; }
-        public List<string> Passports { get; private set; }
-        public List<string> Notes { get; private set; }
-        public List<DateTime> TicketDays { get; private set; }
-        public List<DateTime> TicketIssue { get; private set; }
+        public List<Ticket> Tickets { get; private set; }
         public string Officer { get; private set; }
         public DataReader(string officer, string path)
         {
-            this.GuidID = new List<Guid>();
-            this.Names = new List<string>();
-            this.VehicleIDs = new List<string>();
-            this.Passports = new List<string>();
-            this.Notes = new List<string>();
-            this.TicketDays = new List<DateTime>();
-            this.TicketIssue = new List<DateTime>();
+            this.Tickets = new List<Ticket>();
             this.Officer = officer;
             this.GetDataFromCSV(path);
         }
@@ -34,14 +22,12 @@ namespace Testing
             while((line = streamReader.ReadLine())!=null)
             {
                 string[] row = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
-
-                this.GuidID.Add(Guid.NewGuid());
-                this.TicketIssue.Add(DateTime.Now);
-                this.VehicleIDs.Add(row[0]);
-                this.Passports.Add(row[1]);
-                this.Notes.Add(row[2]);
-                this.TicketDays.Add(DateTime.Parse(row[3]));
-                this.Names.Add(row[4]);
+                string vehicleID = row[0];
+                string passport = row[1];
+                string note = row[2];
+                DateTime ticketDay = DateTime.Parse(row[3]);
+                string name = row[4];
+                this.Tickets.Add(new Ticket(name, vehicleID, passport, note, ticketDay, this.Officer));
             }
         }
 
---
+++ FirstDatabase/Testing/Ticket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing
+{
+    class Ticket
+    {
+        public Guid GuidID { get; private set; }
+        public string Name { get; private set; }
+        public string VehicleID { get; private set; }
+        public string Passport { get; private set; }
+        public string Notes { get; private set; }
+        public DateTime TicketDay { get; private set; }
+        public DateTime TicketIssue { get; private set; }
+        public string Officer { get; private set; }
+        public Ticket(string name, string vehicleId, string passport, string note, DateTime ticketDay, string officer)
+        {
+            this.GuidID = Guid.NewGuid();
+            this.Name = name;
+            this.VehicleID = vehicleId;
+            this.Passport = passport;
+            this.Notes = note;
+            this.TicketDay = ticketDay;
+            this.TicketIssue = DateTime.Today;
+            this.Officer = officer;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{GuidID} | {Name} | {VehicleID} | {Passport} | {Notes} | {TicketDay} | {TicketIssue} | {Officer}");
+        }
+    }
+}
